a9333884ae24ac7c33527316a4bae73d103f432c
Rishab Kumar <rishabkumar7@gmail.com>
2021-01-05 12:28:02 -0500

    Removed interactive particles

--- src/components/ParticleComponent.js
+++ src/components/ParticleComponent.js
@@ -82,45 +82,6 @@ export default function PartcileComponent() {
               },
             },
           },
-          interactivity: {
-            detect_on: "window",
-            events: {
-              onhover: {
-                enable: true,
-                mode: "repulse",
-              },
-              onclick: {
-                enable: false,
-                mode: "bubble",
-              },
-              resize: true,
-            },
-            modes: {
-              grab: {
-                distance: 400,
-                line_linked: {
-                  opacity: 1,
-                },
-              },
-              bubble: {
-                distance: 400,
-                size: 40,
-                duration: 2,
-                opacity: 8,
-                speed: 3,
-              },
-              repulse: {
-                distance: 100,
-                duration: 0.4,
-              },
-              push: {
-                particles_nb: 4,
-              },
-              remove: {
-                particles_nb: 2,
-              },
-            },
-          },
           retina_detect: true,
         }}
       />
